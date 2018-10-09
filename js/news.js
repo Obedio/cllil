4cba9e938164855481a0c1e63868973cedd9f1d5
Obedio Albuquerque <obedioalbuquerque@gmail.com>
2018-10-09 14:45:46 -0300

    Feito correção de books no banner

--- js/news.js
+++ js/news.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
+	let img = $('#news').data("img");
+	let not = $('#news').data("not");;
+
 	let news = [
 	{
-		"img": "https://via.placeholder.com/350x150",
-		"altImg": "noticia",
-		"ahref": "",
-		"titulo": "Teste"
+		"img": "informativo.jpg",
+		"altImg": "informativo",
+		"ahref": "aviso-de-aula-13-10.html",
+		"titulo": "Aviso de Aula!"
 	},
 	{
-		"img": "https://via.placeholder.com/350x150",
-		"altImg": "noticia",
-		"ahref": "",
-		"titulo": "Aberta as Incrições para 2019"
+		"img": "clube-da-conversacao.jpg",
+		"altImg": "Clube da Convesação",
+		"ahref": "clube-da-conversacao.html",
+		"titulo": "Lançamento do Clube da Convesação em Inglês"
 	},
 	{
-		"img": "https://via.placeholder.com/350x150",
-		"altImg": "noticia",
-		"ahref": "",
-		"titulo": "Aberta as Incrições para 2019"
+		"img": "aula-inaugural-libras.jpeg",
+		"altImg": "Aula Inaugural de Libras",
+		"ahref": "aula-inaugural-libras.html",
+		"titulo": "Aula Inaugural de Libras"
 	},
 	{
 		"img": "https://via.placeholder.com/350x150",
@@ -37,13 +40,13 @@ $(document).ready(function () {
 	for(var noticia in news){
 		noticias += 
 		'<div class="card w-100 mb-3">' +
-			'<img class="card-img-top" src="' + news[noticia].img + '" alt="' + news[noticia].altImg + '">' +
+			'<img class="card-img-top" src="' + (img + news[noticia].img) + '" alt="' + news[noticia].altImg + '">' +
 			'<div class="card-body">' +
-				'<a href="' + news[noticia].ahref + '"><h6 class="card-title text-center text-padrao2">' + news[noticia].titulo + '</h6></a>' +
+				'<a href="' + (not + news[noticia].ahref) + '"><h6 class="card-title text-center text-padrao2">' + news[noticia].titulo + '</h6></a>' +
 			'</div>' +
 		'</div>';
 
-		if (noticia == 3)
+		if (noticia == 2)
 			break;
 	}
 
